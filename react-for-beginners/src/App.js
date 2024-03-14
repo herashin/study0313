90a680ca1b521bf4c01bd833ce80c2fbd4cedec0
herashin <https://github.com/herashin/study0313.git>
2024-03-14 13:59:47 +0900

    js 기능 map으로 배열값 불러와 li태그에 동적 리스트 구현하기  (주석추가)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -14,8 +14,8 @@ function App() {
     if (toDo === "") {
       return;
     }
-    setTodo("");
     setTodos((currentArray) => [toDo, ...currentArray]);
+    setTodo("");
   };
   console.log(toDos);
   return (
@@ -30,6 +30,20 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      {/* 
+     ***  to-do list 만들기 
+      map은 배열속의 아이템에 적용하는 자스 함수로
+       뽑아쓸 때  key 값이 필요하다. 
+       뽑아쓰는 내용의 첫번째 argument는 value여야하고,
+       이 value는 현재  toDo를 의미함. 
+        현재 number값이 필요하기때문에  두번째 argument인 index를 사용함.
+      */}
     </div>
   );
 }
