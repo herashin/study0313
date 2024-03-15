3d87e2e4351f0a5509098d3e988f8e4f40cd7e81
herashin <https://github.com/herashin/study0313.git>
2024-03-15 16:35:30 +0900

    코인트래커 api로 불러온 코인 정보를 input으로 숫자(달러)를 입력받아, 선택한 코인을 몇 개 살 수 있는지 구하는 기능 추가

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,48 +2,70 @@ import Button from "./Button";
 import styles from "./App.module.css";
 //import { useState, useEffect } from "react";
 import { func } from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodos((currentArray) => [toDo, ...currentArray]);
-    setTodo("");
+  const [coins, setCoins] = useState([]);
+
+  const [dollar, setDollar] = useState(0);
+  const [btcPrice, setBtcPrice] = useState(null);
+  console.log(btcPrice);
+
+  const selectOnchange = (event) => {
+    setBtcPrice(coins[event.target.value]);
   };
-  console.log(toDos);
+
+  const onChange = (event) => {
+    setDollar(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      //fetchdata("/users")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        setBtcPrice(json[0]);
+      });
+
+    // fetch에 url 삽입가능
+    // cors 에러 확인 , http-proxy-middleware 설치 후  setupProxy.js 파일 생성으로 해결
+    /*  -- 과제 
+     input 생성 후 달러를 입력하면, 선택한 코인을 얼마나 살 수 있는지 알려주는 기능
+     */
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The coins! {loading ? "" : `(${coins.length})`} </h1>
+
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select onChange={selectOnchange}>
+          {coins.map((coin, index) => (
+            <option key={coin.id} value={index}>
+              {coin.name} ({coin.symbol}) : $({coin.quotes.USD.price}) USD
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
         <input
+          value={dollar}
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
+          type="number"
+          placeholder="달러"
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      {/* 
-     ***  to-do list 만들기 
-      map은 배열속의 아이템에 적용하는 자스 함수로
-       뽑아쓸 때  key 값이 필요하다. 
-       뽑아쓰는 내용의 첫번째 argument는 value여야하고,
-       이 value는 현재  toDo를 의미함. 
-        현재 number값이 필요하기때문에  두번째 argument인 index를 사용함.
-      */}
+
+        <hr />
+        <strong>
+          선택한 코인 가격 :{btcPrice.name} ({btcPrice.symbol}) :
+          {dollar / btcPrice.quotes.USD.price}
+        </strong>
+      </div>
     </div>
   );
 }
